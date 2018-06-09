36c31d02b2a31edeecf8514ff51be41b190320b4
takke2 <y_taketsugu@nifty.com>
2018-06-10 01:46:16 +0900

    sa

--- docs/js/app.js
+++ docs/js/app.js
@@ -8,9 +8,10 @@ function connect() {
         services: ['battery_service']
     }]})
     .then(device => device.gatt.connect())
-    //.then(server => server.getPrimaryService('battery_service'))
-    //.then(service => service.getCharacteristic('battery_level'))
+    .then(server => server.getPrimaryService('battery_service'))
+    .then(service => service.getCharacteristic('battery_level'))
     //.then(characteristic => characteristic.readValue())
+    .then(characteristic => alert("here"))
     //.then(value => {
     //  let batteryLevel = value.getUint8(0);
     //  alert( batteryLevel )

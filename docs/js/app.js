e2c92cd882e784f58f0224d69da907d1cdaf9d94
takke2 <y_taketsugu@nifty.com>
2018-06-24 08:27:55 +0900

    k

--- docs/js/app.js
+++ docs/js/app.js
@@ -153,9 +153,9 @@ function init() {
     var enemyMesh = new Array(ENEMY_MAX_COUNT);
     const enemySize = 20;
     for(i=0; i < ENEMY_MAX_COUNT; i++){
-        p.x = 300;
+        p.x = 0;
         p.y = 0;
-        p.z = 0;
+        p.z = 300;
         enemy[i] = new Enemy();
         enemy[i].set(p, enemySize, 0.5);
         enemyMesh[i] = new THREE.Mesh(new THREE.SphereGeometry(enemySize), new THREE.MeshNormalMaterial());

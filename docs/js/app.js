954d3dc5f89480efdb28f72c5cd5f3c4e8357276
takke2 <y_taketsugu@nifty.com>
2018-08-20 23:36:05 +0900

    s

--- docs/js/app.js
+++ docs/js/app.js
@@ -49,7 +49,6 @@ var uart_device;
 var uuid={};
 uuid["UART_SERVICE"]                 ='6E400001-B5A3-F393-E0A9-E50E24DCCA9E';
 uuid["UART_SERVICE_CHARACTERISTICS"] ='6e400003-b5a3-f393-e0a9-e50e24dcca9e';
-const SERVICE_UUID = "6E400001-B5A3-F393-E0A9-E50E24DCCA9E";
 
 function connect(){
     alert("connect‚ª‰Ÿ‚³‚ê‚½");

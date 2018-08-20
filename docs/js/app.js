e8e764ddc3fafd0c31d80478f4ec236040807cd3
takke2 <y_taketsugu@nifty.com>
2018-08-20 23:46:13 +0900

    d

--- docs/js/app.js
+++ docs/js/app.js
@@ -51,7 +51,7 @@ uuid["UART_SERVICE"]                 ='6e400001-b5a3-f393-e0a9-e50e24dcca9e';
 uuid["UART_SERVICE_CHARACTERISTICS"] ='6e400003-b5a3-f393-e0a9-e50e24dcca9e';
 
 function connect(){
-    alert("connect‚ª‰Ÿ‚³‚ê‚½");
+    alert("connect ok");
     
     navigator.bluetooth.requestDevice({
         //acceptAllDevices:true,

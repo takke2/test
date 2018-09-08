9e626d0cf6771228d05b0e7bc099e01e577a68aa
takke2 <y_taketsugu@nifty.com>
2018-09-09 03:03:29 +0900

    f

--- docs/js/app.js
+++ docs/js/app.js
@@ -216,7 +216,7 @@ function init() {
     scene.add( arrowHelper );
 
     conteText2D.clearRect(0, 0, conteText2D.canvas.width, conteText2D.canvas.height);
-    conteText2D.fillText ( "8/25" , 0 , 10 , 100 );
+    conteText2D.fillText ( "9/9" , 0 , 10 , 100 );
     
     bgmplay();
     // アニメーションループ

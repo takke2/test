a2c4bdb5bee1191c87a7a68f73a5d620f95af7e1
takke2 <y_taketsugu@nifty.com>
2018-09-15 08:57:32 +0900

    f

--- docs/js/app.js
+++ docs/js/app.js
@@ -266,7 +266,8 @@ function init() {
     bgmplay();
     // アニメーションループ
     (function loop() {
-        conteText2D.fillText ( counter , 0 , 50 , 100 );
+        conteText2D.clearRect(0, 0, conteText2D.canvas.width, conteText2D.canvas.height);
+        conteText2D.fillText ( counter , 0 , 20 , 100 );
         counter++;
         camerax = camerax + lSpeed + rSpeed;
         cameray = cameray + fSpeed + bSpeed;

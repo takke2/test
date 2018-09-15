ffccaecc95e9eb2da852df8b837f8a95461dafeb
takke2 <y_taketsugu@nifty.com>
2018-09-16 01:22:49 +0900

    s

--- docs/js/app.js
+++ docs/js/app.js
@@ -295,10 +295,8 @@ function init() {
                     scene.add(charaShotMesh[i]);
                     
                     var text = "0,1";
-                    //alert("aaaaaaaaaaa");
                     var arrayBuffe = new TextEncoder("utf-8").encode(text);
-                    //alert(arrayBuffe);
-                    characteristic_rx.writeValue(arrayBuffe);  
+                    //characteristic_rx.writeValue(arrayBuffe);  
   
                     break;
                 }

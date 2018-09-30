cadaf5793eb3caf00bad81ebc7cf9e333a97ae70
takke2 <y_taketsugu@nifty.com>
2018-09-30 17:14:54 +0900

    se

--- docs/js/app.js
+++ docs/js/app.js
@@ -301,7 +301,7 @@ function init() {
                     forwardVec4.normalize();
                     
                     if(i==CHARA_SHOT_MAX_COUNT-1){
-                        charaShot[i].set(camera.position, camera.getWorldDirection().normalize(), 3000, 1000);
+                        charaShot[i].set(camera.position, camera.getWorldDirection().normalize(), 3000, 100);
                     }else{
                         charaShot[i].set(camera.position, camera.getWorldDirection().normalize(), 500, 5);
                     }

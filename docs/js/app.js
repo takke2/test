5bc710673742c795cedea58badcab27d4b449e1b
takke2 <y_taketsugu@nifty.com>
2018-10-28 23:31:37 +0900

    s

--- docs/js/app.js
+++ docs/js/app.js
@@ -411,7 +411,7 @@ function init() {
                 
                 var radian = (counter%360) * Math.PI / 180;
                 if(test1==1){
-                    alert("msg:");
+                    //alert("msg:");
                     enemy[i].position.x = (p.length() * Math.sin(radian));
                     enemy[i].position.z = (p.length() * Math.cos(radian));
                     //test1=0;

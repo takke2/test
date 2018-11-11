ca37ea8d64f604539cd2ab5dfea88d5d10610ac5
takke2 <y_taketsugu@nifty.com>
2018-11-12 01:38:15 +0900

    s

--- docs/js/app.js
+++ docs/js/app.js
@@ -34,7 +34,7 @@ var isStart=0;
 var isGoo=0;
 var isKonami=0;
 
-window.onload = function() { setTimeout(scrollTo, 100, 0, 1); }
+window.scrollTo(0,1);
 
 function sleep(waitMsec) {
   var startMsec = new Date(); 
@@ -335,7 +335,7 @@ function init() {
 
     conteText2D.clearRect(0, 0, conteText2D.canvas.width, conteText2D.canvas.height);
     conteText2D.fillStyle = "blue";
-    conteText2D.fillText ( "10/27-4" , 0 , 10 , 100 );
+    conteText2D.fillText ( "11/8" , 0 , 10 , 100 );
     conteText2D.fillText ( "hp:"+hp , conteText2D.canvas.width/2 , 10 , 100 );
     conteText2D.fillText ( "e:"+enemy_count , conteText2D.canvas.width/2 , 20 , 100 );
     

24254791e2d4b78cd2ba51f63cceb5abde11e80b
takke2 <y_taketsugu@nifty.com>
2019-02-17 06:29:40 +0900

    s

--- docs/js/app.js
+++ docs/js/app.js
@@ -271,9 +271,9 @@ function init() {
         skyboxMaterial
     );
 
-    var zimengeometory = new THREE.BoxGeometry(10000, 10000, 10000);
+    var zimengeometory = new THREE.BoxGeometry(10000, 10+9990, 10000);
     var zimenmesh = new THREE.Mesh(zimengeometory, zimen);
-    zimenmesh.position.set(0, -10030, -30);
+    zimenmesh.position.set(0, -30-9990, -30);
     
     scene.add(zimenmesh);
     scene.add(skybox);

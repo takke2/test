831947febf20e86d7256b8758d65b00a1c76c017
takke2 <y_taketsugu@nifty.com>
2019-02-16 14:17:55 +0900

    s

--- docs/js/app.js
+++ docs/js/app.js
@@ -402,7 +402,7 @@ function init() {
                 conteText2D.clearRect(0, 0, conteText2D.canvas.width, conteText2D.canvas.height);
                 conteText2D.fillText ( "hp:"+hp , conteText2D.canvas.width/2-10 , 10 , 100 );
                 conteText2D.fillText ( "e:"+enemy_count , conteText2D.canvas.width/2-10 , 20 , 100 );
-                conteText2D.fillText ( "time:"+lastkeika , conteText2D.canvas.width/2-10 , 30 , 100 );
+                conteText2D.fillText ( "time:"+(90-lastkeika) , conteText2D.canvas.width/2-10 , 30 , 100 );
                 
             }
             

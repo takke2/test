91787c03a1b7d5c0b70e045c93496299e9cc05ec
takke2 <y_taketsugu@nifty.com>
2019-02-16 14:19:38 +0900

    s

--- docs/js/app.js
+++ docs/js/app.js
@@ -493,6 +493,7 @@ function init() {
                         conteText2D.clearRect(0, 0, conteText2D.canvas.width, conteText2D.canvas.height);
                         conteText2D.fillText ( "hp:"+ hp , conteText2D.canvas.width/2-10 , 10 , 100 );
                         conteText2D.fillText ( "e:"+ enemy_count , conteText2D.canvas.width/2-10 , 20 , 100 );
+                        conteText2D.fillText ( "time:"+(90-lastkeika) , conteText2D.canvas.width/2-10 , 30 , 100 );
                         text = "0,4";
                         arrayBuffe = new TextEncoder("utf-8").encode(text);
                         if(auto==0){
@@ -525,6 +526,7 @@ function init() {
                                 conteText2D.clearRect(0, 0, conteText2D.canvas.width, conteText2D.canvas.height);
                                 conteText2D.fillText ( "hp:"+hp , conteText2D.canvas.width/2-10 , 10 , 100 );
                                 conteText2D.fillText ( "e:"+enemy_count , conteText2D.canvas.width/2-10 , 20 , 100 );
+                                conteText2D.fillText ( "time:"+(90-lastkeika) , conteText2D.canvas.width/2-10 , 30 , 100 );
                                 scene.remove(enemyMesh[j]);
                                 charaShot[i].alive = false;
                                 explay();

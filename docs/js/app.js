a82b709a4cba2f3899f20253bf328db1834b8dd0
takke2 <y_taketsugu@nifty.com>
2019-02-17 06:58:09 +0900

    d

--- docs/js/app.js
+++ docs/js/app.js
@@ -273,7 +273,7 @@ function init() {
 
     var zimengeometory = new THREE.BoxGeometry(10000, 10+9990, 10000);
     var zimenmesh = new THREE.Mesh(zimengeometory, zimen);
-    zimenmesh.position.set(0, -1000, -30);
+    zimenmesh.position.set(0, -5030, -30);
     
     scene.add(zimenmesh);
     scene.add(skybox);
@@ -375,6 +375,7 @@ function init() {
         //characteristic_rx.writeValue(arrayBuffe);
         
         if(auto==1){
+            tStart = performance.now();
             isStart = 1;
             counter+=1;
         

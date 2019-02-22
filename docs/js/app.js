422aa58efb7564a77d62996c52efecdec4588137
takke2 <y_taketsugu@nifty.com>
2019-02-23 03:29:09 +0900

    e

--- docs/js/app.js
+++ docs/js/app.js
@@ -474,8 +474,8 @@ function init() {
                         }
                         
                         if(i==0){
-                            conteText2D.clearRect(0, 0, conteText2D.canvas.width, conteText2D.canvas.height);
-                            conteText2D.fillText ( "r:"+charaShot[0].position.x) , conteText2D.canvas.width/2-100 , 40 , 150 );
+                            //conteText2D.clearRect(0, 0, conteText2D.canvas.width, conteText2D.canvas.height);
+                            //conteText2D.fillText ( "r:"+charaShot[0].position.x) , conteText2D.canvas.width/2-100 , 40 , 150 );
                         }
                         
                         charaShotMesh[i].position.set(charaShot[i].position.x, charaShot[i].position.y,charaShot[i].position.z);
